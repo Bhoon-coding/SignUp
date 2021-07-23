339f82a2444d0301d203cfbffd472d93ad1b031e
이병훈 <bhlee@onns.co.kr>
2021-07-23 16:34:28 +0900

    learned modal, Add ImagePickerViewController

--- SignUp/SecondViewController.swift
+++ SignUp/SecondViewController.swift
@@ -8,15 +8,45 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        }
+        print("SecondVIewController의 view가 메모리에 로드 됨")
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("SecondViewController의 view가 화면에 보여짐")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("SecondViewController의 view가 화면에서 사라질 예정")
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("SecondViewController의 view가 화면에서 사라짐")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        print("SecondViewController의 view가 subview를 레이아웃 하려함")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        print("SecondViewController의 view가 subview를 레이아웃 함")
+    }
 
     @IBAction func popToView () {
         self.navigationController?.popViewController(animated: true)
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func dismissModal () {
+        self.dismiss(animated: true, completion: nil)
     }
     
 
@@ -31,3 +61,4 @@ class SecondViewController: UIViewController {
     */
 
 }
+ 
