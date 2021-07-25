2a05255400dc6b4011623165be269f3876aa20fc
이병훈 <bhlee@onns.co.kr>
2021-07-26 08:50:36 +0900

    tapGesture using by delegate

--- SignUp/TapGesture.swift
+++ SignUp/TapGesture.swift
@@ -6,17 +6,23 @@
 //
 
 import UIKit
-
-class TapGestureViewController: UIViewController {
-    @IBAction func tapView(_ sender: UITapGestureRecognizer) {
-        self.view.endEditing(true)
-    }
+    // 다양한 상황에서 애니메이션을 사용하고 싶다면 Delegate를 사용 !!
+class TapGestureViewController: UIViewController, UIGestureRecognizerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.tapView(_:)))
-        
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer()
+        tapGesture.delegate = self
         self.view.addGestureRecognizer(tapGesture)
+        
+        // tapGesture using by code
+//        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.tapView(_:)))
+//
+//        self.view.addGestureRecognizer(tapGesture)
+    }
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        self.view.endEditing(true)
+        return false
     }
 }
