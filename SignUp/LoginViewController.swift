7fbf58e5f4b83108ca59bbdb5246f0b5dbbb08ec
이병훈 <bhlee@onns.co.kr>
2021-07-26 22:44:55 +0900

    add app logo image, sign up layout

---
+++ SignUp/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  SignUp
+//
+//  Created by BH on 2021/07/26.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func touchSignUpButton () {
+        
+    }
+}
